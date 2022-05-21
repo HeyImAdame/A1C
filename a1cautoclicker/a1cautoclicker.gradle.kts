a415a53faa651844f49931f10f57a0abcd08ee70
HeyImAdame <cakonarik@gmail.com>
2022-05-20 19:40:31 -0500

    this 1

--- a1cautoclicker/a1cautoclicker.gradle.kts
+++ a1cautoclicker/a1cautoclicker.gradle.kts
@@ -24,7 +24,7 @@ import ProjectVersions.rlVersion
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+//
 version = "1337.0.0"
 
 project.extra["PluginName"] = "AoneC AutoClicker"
